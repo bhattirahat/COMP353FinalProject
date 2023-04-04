d2ce8efa75dc2ec98ffc74630703221fe108057f
devinKuriya <65865454+devinKuriya@users.noreply.github.com>
2023-04-04 16:31:56 -0400

    Vaccination changes

--- src/routes/vaccination_routes.js
+++ src/routes/vaccination_routes.js
@@ -1,4 +1,4 @@
-const express = require('express');
+ const express = require('express');
 const router = express.Router();
 const path = require('path')
 const rootDir = require('../util/path');
@@ -6,16 +6,12 @@ const { StatusCode } = require('status-code-enum')
 const db = require('../util/database');
 
 
+
 // GET Vaccination information
 router.get('/', async (req, res, next) => {
-    
-
     getFacility = `SELECT * from Vaccination;`
     //getFacilityType = `SELECT type from Vaccine;`
     get = getFacility;//+getFacilityType;
-    
-
-
      db.then(conn => {
         conn.query(get, (err, result, fields) => {
             if (err) {
@@ -40,25 +36,6 @@ router.get('/', async (req, res, next) => {
         console.log(err)
     })
 })
-
-router.post('/',async(req,res,next)=>{
-
-    var id = req.body.Vaccination_id;
-    
-     db.then(conn => {
-        conn.query(`Delete from Vaccination where Vaccination_id="${id}"` , async (err, result, fields) => {
-            if (err) {
-                throw err;
-            }
-            res.status(StatusCode.SuccessOK)
-                .render('vaccination', {
-                 pageTitle: 'Vaccination' , success:'', data: totalVaccination , field:totaltype
-                        
-                })
-    }).catch(err => {
-        console.log(err)
-    })
-})})
 
 
 router.get('/', async (req, res, next) => {
