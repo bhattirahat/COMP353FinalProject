5899659191909e6db579cbd24abe009f32243d3b
CSman22 <jackey211@hotmail.com>
2023-04-05 12:26:50 -0400

    css changes

--- src/routes/employee_routes.js
+++ src/routes/employee_routes.js
@@ -8,8 +8,26 @@ const db = require('../util/database')
 // Query #
 // GET employee information
 router.get('/', async (req, res, next) => {
+    query = `
+        select 
+            t1.employee_id, 
+            t1.first_name,
+            t1.last_name,
+            t1.date_of_birth,
+            t1.medical_card_number,
+            t1.is_citizenship,
+            t1.email,
+            t2.Address_line,
+            t2.Postal_Code,
+            t3.telephone_number,
+            t4.Type as occupation
+        from Employee t1
+        inner join Employee_Address t2 on t1.employee_id = t2.employee_id
+        inner join Employee_Telephone t3 on t1.employee_id = t3.employee_id
+        inner join Occupation t4 on t4.occupation_id = t1.occupation_id;
+        `
     db.then(conn => {
-        conn.query(`SELECT * from Employee`, (err, result, fields) => {
+        conn.query(query, (err, result, fields) => {
             if (err) {
                 throw err;
             }
