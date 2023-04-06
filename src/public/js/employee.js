6ed1bd991d5a3c26b35362c0cdc52f74d1ff67ab
CSman22 <jackey211@hotmail.com>
2023-04-06 01:55:27 -0400

    Added add/update/delete to the employee page

--- src/public/js/employee.js
+++ src/public/js/employee.js
@@ -25,50 +25,126 @@ $(document).ready(function () {
         }
     });
 
+
+    // Add users
+    $('#btnCreateEmp').click(function () {
+        var firstName = $("#firstNameAdd").val();
+        var lastName = $("#lastNameAdd").val();
+        var email = $("#emailAdd").val();
+        var medCardNum = $("#medCardNumAdd").val();
+        var telephone = $("#telephoneAdd").val();
+        var dob = $("#dobAdd").val();
+        var occupationId = $("#occupationIdAdd").val();
+        var address = $("#addressAdd").val();
+        var postalCode = $("#postalCodeAdd").val();
+        var citizenship = $("#citizenshipAdd").is(":checked");
+        $.ajax({
+            type: "POST",
+            url: `/employee/create`,
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                medCardNum: medCardNum,
+                telephone: telephone,
+                dob: dob,
+                occupationId: occupationId,
+                address: address,
+                postalCode: postalCode,
+                citizenship: citizenship
+            },
+            success: function (response) {
+                window.location.reload()
+            },
+            error: function (xhr, status, error) {
+                console.error('POST request failed:', status, error);
+            }
+        });
+    });
+
     $('#btnEdit').click(function () {
         var selectedRow = $('#employee_table tbody tr.selected');
 
         if (selectedRow.length) {
             var rowData = table.row(selectedRow).data();
+            $("#empIdEdit").val(rowData[0]);
+            $("#firstNameEdit").val(rowData[1]);
+            $("#lastNameEdit").val(rowData[2]);
+            $("#emailEdit").val(rowData[3]);
+            $("#medCardNumEdit").val(rowData[5]);
+            $("#telephoneEdit").val(rowData[10]);
+            $("#addressEdit").val(rowData[8]);
+            $("#postalCodeEdit").val(rowData[9]);
+            if (rowData[6] == 1) {
+                $('#citizenshipEdit').prop('checked', true);
+            } else {
+                $('#citizenshipEdit').prop('checked', false);
+            }
             console.log(rowData);
         } else {
             console.log('No row is selected.');
         }
     });
 
-
     // save the row being edited 
-    $('#btnSave').on('click', function () {
-        var selectedRows = {};
-
-        table.rows().nodes().to$().each(function (index, element) {
-            var checkbox = $(element).find('.select-checkbox');
-            if (checkbox.is(':checked')) {
-                var rowData = table.row($(element)).data();
-                // selectedRows.push(rowData);
-                console.log(rowData);
-                selectedRows[index] = {
-                    emp_id: rowData[1],
-                    first_name: rowData[2],
-                    last_name: rowData[3],
-                };
-            }
-        });
-
-        console.log(selectedRows);
-
+    $('#btnSaveEdit').on('click', function () {
+        var empId = $("#empIdEdit").val();
+        var firstName = $("#firstNameEdit").val();
+        var lastName = $("#lastNameEdit").val();
+        var email = $("#emailEdit").val();
+        var medCardNum = $("#medCardNumEdit").val();
+        var telephone = $("#telephoneEdit").val();
+        var dob = $("#dobEdit").val();
+        var occupationId = $("#occupationIdEdit").val();
+        var address = $("#addressEdit").val();
+        var postalCode = $("#postalCodeEdit").val();
+        var citizenship = $("#citizenshipEdit").is(":checked");
+        console.log(empId)
         $.ajax({
-            url: '/employee/edit',
-            type: 'PUT',
-            data: JSON.stringify(selectedRows),
-            contentType: 'application/json',
+            type: "PUT",
+            url: `/employee/edit/${empId}`,
+            data: {
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                medCardNum: medCardNum,
+                telephone: telephone,
+                dob: dob,
+                occupationId: occupationId,
+                address: address,
+                postalCode: postalCode,
+                citizenship: citizenship
+            },
             success: function (response) {
-                console.log('PUT request succeeded:', response);
+                window.location.reload()
             },
             error: function (xhr, status, error) {
-                console.error('PUT request failed:', status, error);
+                console.error('POST request failed:', status, error);
             }
         });
+    });
+
+    $('#btnDeleteConfirmation').click(function () {
+        var selectedRow = $('#employee_table tbody tr.selected');
+
+        if (selectedRow.length) {
+            var rowData = table.row(selectedRow).data();
+            var id = rowData[0];
+            $.ajax({
+                url: `/employee/delete/${id}`,
+                type: 'DELETE',
+                contentType: 'application/json',
+                success: function (response) {
+                    console.log('DELETE request succeeded:', response);
+                    window.location.reload()
+                },
+                error: function (xhr, status, error) {
+                    console.error('DELETE request failed:', status, error);
+                }
+            });
+        } else {
+            console.log('No row is selected.');
+        }
     });
 });
 
--- src/routes/facility_routes.js
+++ src/routes/facility_routes.js
@@ -8,51 +8,159 @@ const db = require('../util/database')
 // Query #
 // GET employee information
 router.get('/', async (req, res, next) => {
-    res
-        .status(StatusCode.SuccessOK)
-        .render('facility', {
-            pageTitle: 'Facility', success: ''
-        })
-})
-router.post('/', async (req, res, next) => {
 
-    var facility = req.body.facname;
-    var type = 1
-    var address = req.body.address;
-    var city = 1;
-    var postalcode = req.body.postalcode;
-    var phonenumber = req.body.phonenumber;
-    var webaddress = req.body.webaddress;
-    var capacity = req.body.Capacity
 
-    var send =
-        `Insert Into Facility(name,facility_type_id,address,city_id,postal_code,phone_number,web_address,capacity)
-     values("${facility}",${type},"${address}",${city},"${postalcode}","${phonenumber}","${webaddress}",${capacity})`
+    getFacility = `SELECT * from Facility;`
+    getFacilityType = `SELECT type from Facility_Type;`
+    get = getFacility + getFacilityType;
+
+
 
     db.then(conn => {
-        conn.query(send, (err, result, fields) => {
+        conn.query(get, (err, result, fields) => {
             if (err) {
                 throw err;
             }
             console.log("SQL Query Result-- ", result);
             if (result.length !== 0) {
-                result = result[0];
-                // res.redirect("/facility")
-                res.render('facility', {
-                    pageTitle: 'Facility', success: 'Success'
-                })
+                totalFacility = result[0]
+                console.log("total" + totalFacility)
+                totaltype = result[1]
+                console.log(totaltype)
+                res.status(StatusCode.SuccessOK)
+                    .render('facility', {
+                        pageTitle: 'Facility', success: '', data: totalFacility, field: totaltype
+                    }) //perform your required work on result
             }
-            conn.end();
+            // conn.end();
         });
     }).catch(err => {
         console.log(err)
     })
 
 
+
+
+
+    // db.then(conn => {
+    //     conn.query(`SELECT type from Facility_Type`, (err, result, fields) => {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         console.log("SQL Query Result-- ", result);
+    //         if (result.length !== 0) {
+
+    //             res.status(StatusCode.SuccessOK)
+    //             .render('facility', {
+    //              pageTitle: 'Facility' , success:'', data: result
+    //                          }) //perform your required work on result
+    //         }
+    //         // conn.end();
+    //     });
+    // }).catch(err => {
+    //     console.log(err)
+    // })  
 })
 
-// router.get("/",async(req,res,next)=>{
 
-// })
+router.post('/', async (req, res, next) => {
+
+    var facility = req.body.facname;
+    var type = req.body.type //from facility_type
+    var address = req.body.address;
+    var city = 1; //from city table
+    var postalcode = req.body.postalcode;
+    var phonenumber = req.body.phonenumber;
+    var webaddress = req.body.webaddress;
+    var capacity = req.body.Capacity
+
+
+
+    db.then(conn => {
+        conn.query(`Select facility_type_id from Facility_Type where type= "${type}"`, async (err, result, fields) => {
+            if (err) {
+                throw err;
+            }
+            //console.log("SQL Query Result-- ", result);
+            if (result.length !== 0) {
+                result = result[0];
+                factype = await result["facility_type_id"]
+
+                var send =
+                    `Insert Into Facility(name,facility_type_id,address,postal_code,phone_number,web_address,capacity)
+                values("${facility}",${factype},"${address}","${postalcode}","${phonenumber}","${webaddress}",${capacity})`
+
+                //query to insert into Facility Table
+                db.then(conn => {
+                    conn.query(send, (err, result, fields) => {
+                        if (err) {
+                            throw err;
+                        }
+                        // console.log("SQL Query Result-- ", result);
+                        if (result.length !== 0) {
+                            res.redirect(req.originalUrl)
+                            // res.render('facility', {
+                            //     pageTitle: 'Facility' , success:'Success'
+                            // })
+                        }
+                        // conn.end();
+                    });
+                }).catch(err => {
+                    console.log(err)
+                })
+            }
+            // conn.end();
+        });
+    }).catch(err => {
+        console.log(err)
+    })
+
+    // router.get('/data', async (req, res, next) =>{
+
+    //     res
+    //     .status(StatusCode.SuccessOK)
+    //     .render('facilitydata', {
+    //         pageTitle: 'Facility Data'
+    //     })
+    // })
+
+    // var send = 
+    // `Insert Into Facility(name,facility_type_id,address,city_id,postal_code,phone_number,web_address,capacity)
+    //  values("${facility}",${type},"${address}",${city},"${postalcode}","${phonenumber}","${webaddress}",${capacity})`
+
+    //  console.log(send)
+
+
+    //  db.then(conn => {
+    //     conn.query(send, (err, result, fields) => {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         console.log("SQL Query Result-- ", result);
+    //         if (result.length !== 0) {
+    //             result = result[0];
+    //             // res.redirect("/facility")
+    //             res.render('facility', {
+    //                 pageTitle: 'Facility' , success:'Success'
+    //             })
+
+
+    //         }
+    //         // conn.end();
+    //     });
+    // }).catch(err => {
+    //     console.log(err)
+    // })
+
+})
+
+router.get("/data", async (req, res, next) => {
+
+
+    res.status(StatusCode.SuccessOK)
+        .render('facilitydata', {
+            pageTitle: 'Facility'
+        })
+})
 
 module.exports = router
