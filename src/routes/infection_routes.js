60ac778861bb4ac443a87dfabd2ac003bf1d2992
KenjiDuggan <kenjiduggan@gmail.com>
2023-04-07 21:26:38 -0400

    add infection fetch all and infection table ui

--- src/routes/infection_routes.js
+++ src/routes/infection_routes.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path')
-const rootDir = require('../util/path');
 const { StatusCode } = require('status-code-enum')
+const db = require('../util/database');
+ 
+// GET Infection data and render the infection table
+router.get('/', async (req, res) => {
+    selectAll = `SELECT a.date, a.employee_id, a.infection_id, b.type FROM infection a INNER JOIN infection_type b ON a.infection_type_id=b.infection_type_id`
+    db.execute(
+        selectAll,
+        function(err, data) {
+            if(err) throw err;
+            if(data.length >= 1) {
+                res.status(StatusCode.SuccessOK)
+                .render('infection', {
+                    pageTitle: 'Infection',
+                    success: '',
+                    data: data
+            }) 
+            } else {
+                console.log("No results were returned.");
+            }
+        }
+      );
+});
 
-// Query #
-// GET employee information
-router.get('/', async (req, res, next) => {
-    res
-        .status(StatusCode.SuccessOK)
-        .render('infection', {
-            pageTitle: 'Infection'
-        })
-})
-
-module.exports = router
+module.exports = router;
